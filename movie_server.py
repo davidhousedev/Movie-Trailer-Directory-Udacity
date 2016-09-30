b1f2a103c947e1b443eb02c2ed72cf67ffc86825
David House <davidhousedev@gmail.com>
2016-09-30 10:26:54 -0400

    Successfully implemented custom do_GET()

--- movie_server.py
+++ movie_server.py
@@ -6,13 +6,17 @@ import SocketServer
 from movie import Movie
 from tvshow import TvShow
 
+# borrowed MyRequestHandler syntax from: http://stackoverflow.com/questions/10607621/a-simple-website-with-python-using-simplehttpserver-and-socketserver-how-to-onl
+class MyRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+        	self.path = '/test.html'
+        return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+
 PORT = 8000
 
-Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
-httpd = SocketServer.TCPServer(("", PORT), Handler)
+Handler = MyRequestHandler
+httpd = SocketServer.TCPServer(("0.0.0.0", PORT), Handler)
 
 print "serving at port", PORT
 httpd.serve_forever()
-
-do_GET():
-	got = TvShow("game of thrones", "http://www.google.com")
